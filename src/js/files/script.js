e09c69c2e359f16094dda8efb2e910aabcb68c3f
Dima <uvarov3123@gmail.com>
2023-08-17 18:15:41 +0300

    add ratings

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,23 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+window.addEventListener('load', pageLoad)
+
+function pageLoad() {
+    const htmlTag = document.documentElement
+
+    document.addEventListener("click", (e) => {
+        const targetElement = e.target;
+
+        if (targetElement.closest('.sorting') && htmlTag.closest('.touch')) {
+            targetElement.closest('.sorting').classList.toggle('active')
+        }
+
+        if (!targetElement.closest('.sorting') && document.querySelector('.sorting.active')) {
+            document.querySelector('.sorting.active').classList.remove('active')
+        }
+
+    });
+}
